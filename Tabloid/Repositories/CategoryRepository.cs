0ad4addf2ea8dca9bc603eb84c9804c10f4be634
Nicholas Douso <nicholas.douso@gmail.com>
2021-04-16 14:34:08 -0500

    commit to checkout PR

---
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Category> GetAllCategories()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name]
+                        FROM Category
+                        ORDER BY [Name]
+                    ";
+
+                    var reader = cmd.ExecuteReader();
+                    var categories = new List<Category>();
+
+                    while (reader.Read())
+                    {
+                        categories.Add(NewCatFromDb(reader));
+                    }
+                    reader.Close();
+                    return categories;
+                }
+            }
+        }
+        private Category NewCatFromDb(SqlDataReader reader)
+        {
+            return new Category()
+            {
+                Id = DbUtils.GetInt(reader, "Id"),
+                Name = DbUtils.GetString(reader, "Title"),
+
+            };
+        }
+
+    }
+}
