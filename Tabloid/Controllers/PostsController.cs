e518dec93b37d94fdbee01187dea54cadaf5db11
Nicholas Douso <nicholas.douso@gmail.com>
2021-04-16 09:46:24 -0500

    controller addition

---
+++ Tabloid/Controllers/PostsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Repositories;
+
+namespace Tabloid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostsController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+       return Ok(_postRepository.GetAllPosts());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+        {
+            var post = _postRepository.GetPostById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+    }
+}
