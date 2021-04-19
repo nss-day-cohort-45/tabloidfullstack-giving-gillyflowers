dd6ea40fdd9a597da5d8154e7e3ed82961c5f014
Aaron Resch <aaronmresch@gmail.com>
2021-04-19 13:31:43 -0500

    saving progress for merge

--- Tabloid/Repositories/UserProfileRepository.cs
+++ Tabloid/Repositories/UserProfileRepository.cs
@@ -35,6 +35,24 @@ namespace Tabloid.Repositories
             }
         }
 
+        public UserProfile GetByUserProfileId(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT up.Id, Up.FirebaseUserId, up.FirstName, up.LastName, up.DisplayName, 
+                               up.Email, up.CreateDateTime, up.ImageLocation, up.UserTypeId,
+                               ut.Name AS UserTypeName
+                          FROM UserProfile up
+                               LEFT JOIN UserType ut on up.UserTypeId = ut.Id
+                         WHERE up.Id = @Id";
+                }
+            }
+        }
+
         public UserProfile GetByFirebaseUserId(string firebaseUserId)
         {
             using (var conn = Connection)
