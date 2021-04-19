0cf7792b933e8ef23fcc596774ea6d198af0c1b8
anna-moore <ammoore434@gmail.com>
2021-04-19 11:30:19 -0500

    start comment repo

---
+++ Tabloid/Repositories/CommentRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Models;
+using Tabloid.Utils;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Tabloid.Repositories
+{
+    public class CommentRepository : BaseRepository
+    {
+        public CommentRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Comment> GetAllComments()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using( var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT 
+                                        FROM Comments c
+                                        WHERE ";
+                }
+            }
+        }
+    }
+}
